201add47d49b27d470a14f53a1def78b5083ec79
sonalishinde30 <shindesp2001@gmail.com>
2024-09-20 15:29:31 +0530

    pushing complete code

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import { BrowserRouter ,Routes,Route } from "react-router-dom";
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Menu from './pages/Menu' 
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/about" element={<About/>} />
+        <Route path="/contact" element={<Contact/>} />
+        <Route path="/menu" element={<Menu/>} />
+        <Route path="*" element={<Home/>} />
+      </Routes>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+
+export default App;
